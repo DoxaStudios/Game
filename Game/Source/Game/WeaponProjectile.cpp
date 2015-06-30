5acd9744587524e62e6c4a723453c9a0bf4d8e53
Chad Reddick <chad_reddick2008@yahoo.com>
2015-06-29 20:36:40 -0500

    Added Weapon Projectile and Functionality

---
+++ Game/Source/Game/WeaponProjectile.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game.h"
+#include "WeaponProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+AWeaponProjectile::AWeaponProjectile(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// Use a sphere as a simple collision representation
+	CollisionComp = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(5.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("DefaultProjectile");
+	CollisionComp->OnComponentHit.AddDynamic(this, &AWeaponProjectile::OnHit);		// set up a notification for when this component hits something blocking
+
+	// Players can't walk on it
+	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	CollisionComp->CanCharacterStepUpOn = ECB_No;
+
+	// Set as root component
+	RootComponent = CollisionComp;
+
+	// Use a ProjectileMovementComponent to govern this projectile's movement
+	ProjectileMovement = ObjectInitializer.CreateDefaultSubobject<UProjectileMovementComponent>(this, TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+
+	// Die after 3 seconds by default
+	InitialLifeSpan = 3.0f;
+
+}
+
+void AWeaponProjectile::OnHit(AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+
+}
---
+++ Game/Source/Game/WeaponProjectile.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "WeaponProjectile.generated.h"
+
+UCLASS()
+class GAME_API AWeaponProjectile : public AActor
+{
+	GENERATED_BODY()
+
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	class USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement;
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponProjectile(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(EditDefaultsOnly, Category = Explosion)
+		UParticleSystem* Explosion;
+
+	/** called when projectile hits something */
+	UFUNCTION()
+	virtual void OnHit(AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	/** Returns CollisionComp subobject **/
+	FORCEINLINE class USphereComponent* GetCollisionComp() const { return CollisionComp; }
+	/** Returns ProjectileMovement subobject **/
+	FORCEINLINE class UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+
+	
+	
+};
